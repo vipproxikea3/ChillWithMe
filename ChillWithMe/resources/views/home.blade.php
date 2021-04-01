127292f1f6fa5710d066612c22dadeb2d8794532
Dong Quang Quy <quangquya2@gmail.com>
2021-04-02 00:43:15 +0700

    update - views:home.blade.php

--- ChillWithMe/resources/views/home.blade.php
+++ ChillWithMe/resources/views/home.blade.php
@@ -2,11 +2,21 @@
 
 @section('content')
 <div class="container">
-    <div class="row  card-profile p-5 mt-5">
+    <div class="row">
+        <form action="/rooms" method="GET">
+            <input style="display: none;" type="text" name="idRoom" value="{{Auth::user()->id}}">
+            <button type="submit" id="myRoom" class="ml-3 btn btn-outline-success">
+                <i class="fa fa-arrow-right mr-3" aria-hidden="true"></i>
+                <span>Phòng của tôi</span>
+            </button>
+        </form>
+
+    </div>
+    <div class="row card-profile p-5 mt-5">
         <div class="col">
             <div class="row justify-content-center my-3">
                 <div class="col-lg-3 col-sm-6">
-                    <div class="profile-field font-weight-bold py-1 px-3">Họ và tên</div>
+                    <div class="profile-field py-1 px-3">Họ và tên</div>
                 </div>
                 <div class="col-lg-3 col-sm-6">
                     <span>{{Auth::user()->name}}</span>
@@ -14,7 +24,7 @@
             </div>
             <div class="row justify-content-center my-3">
                 <div class="col-lg-3 col-sm-6">
-                    <div class="profile-field font-weight-bold py-1 px-3">Tên đăng nhập</div>
+                    <div class="profile-field py-1 px-3">Tên đăng nhập</div>
                 </div>
                 <div class="col-lg-3 col-sm-6">
                     <span>{{Auth::user()->email}}</span>
@@ -22,7 +32,7 @@
             </div>
             <div class="row justify-content-center my-3">
                 <div class="col-lg-3 col-sm-6">
-                    <div class="profile-field font-weight-bold py-1 px-3">Mã phòng</div>
+                    <div class="profile-field py-1 px-3">Mã phòng</div>
                 </div>
                 <div class="col-lg-3 col-sm-6">
                     <span>{{Auth::user()->idRoom}}</span>
@@ -30,7 +40,7 @@
             </div>
             <div class="row justify-content-center my-3">
                 <div class="col-lg-3 col-sm-6">
-                    <div class="profile-field font-weight-bold py-1 px-3">Mật khẩu</div>
+                    <div class="profile-field py-1 px-3">Mật khẩu</div>
                 </div>
                 <div class="col-lg-3 col-sm-6">
                     <div><a href="">Đổi mật khẩu</a></div>
--- ChillWithMe/resources/views/layouts/app.blade.php
+++ ChillWithMe/resources/views/layouts/app.blade.php
@@ -20,6 +20,9 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
+    <!-- font-awesome icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
+
     <!-- Styles -->
     <link href="/css/app.css" rel="stylesheet">
 
